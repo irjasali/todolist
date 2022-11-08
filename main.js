b34a9b0e7ea9b035078cdc4c587db8253db3ff92
Irving J. S. Linares <irjasali@gmail.com>
2022-11-07 19:53:32 -0600

    Firt Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const todos = [];
+const render = () => {
+  const todoList = document.getElementById("todo-list");
+  const todosTemplate = todos.map((t) => "<li>" + t + "</li>");
+  todoList.innerHTML = todosTemplate.join("");
+  const elementos = document.querySelectorAll("#todo-list li");
+  elementos.forEach((elemento, i) => {
+    elemento.addEventListener("click", () => {
+      elemento.parentNode.removeChild(elemento);
+      todos.splice(i, 1);
+      render();
+    });
+  });
+};
+window.onload = () => {
+  const form = document.getElementById("todo-form");
+  form.onsubmit = (e) => {
+    e.preventDefault();
+    const todo = document.getElementById("todo");
+    const todoText = todo.value;
+    todo.value = "";
+    todos.push(todoText);
+    render();
+  };
+};
